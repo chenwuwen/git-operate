442a8e372fdde6b42befee52d7a8bcc9a9cbd292
chenwuwen <2504954849@qq.com>
2022-08-25 22:34:27 +0800

    Update GitOps.java

--- src/main/java/cn/kanyun/GitOps.java
+++ src/main/java/cn/kanyun/GitOps.java
@@ -153,7 +153,7 @@ public class GitOps {
 
     /**
      * git 迁移,虽然是git clone命令,但是下载的文件,并不是你能直接使用文件,而是需要push到新仓库才能用
-     * https://mp-new.csdn.net/mp_blog/creation/editor/116749871
+     * https://blog.csdn.net/kanyun123/article/details/116749871
      * @param remoteRepoPath
      * @param localRepoPath
      * @param userName
